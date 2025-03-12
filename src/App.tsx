bfd22ca4873f9952f83c04f36ec480adc2c04b56
Carlos Iure <carlos.iure1403@gmail.com>
2025-03-12 13:49:55 -0300

    [ADD] libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
+import toast, {Toaster} from 'react-hot-toast'
+import {BoxLine,Btn} from '@ciure/oksnk-ui'
 
 function App() {
   return (
     <>
-      <div>
+      <Toaster/>
+      <BoxLine direction='column' >
         <h1> Ci Developer</h1>
-      </div>
+        <Btn color='black' onClick={()=> toast.success("click ! ")}> Click</Btn>
+      </BoxLine>
     </>
   )
 }
