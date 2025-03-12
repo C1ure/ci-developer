401ad0ba6dc4265d26b6119a3b15b617995847a7
Carlos Iure <carlos.iure1403@gmail.com>
2025-03-12 13:57:09 -0300

    [ADD] Strutct site

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,21 @@ function App() {
   return (
     <>
       <Toaster/>
-      <BoxLine direction='column' >
+      <BoxLine height='10vh' bg_color='black' position='absolute' left ='0px' top='0px' margin='0px 0px 15vh 0px '>
+        <h3> Nav Bar</h3>
+      </BoxLine>
+      <BoxLine direction='column' height='100vh'>
         <h1> Ci Developer</h1>
         <Btn color='black' onClick={()=> toast.success("click ! ")}> Click</Btn>
+      </BoxLine>
+      <BoxLine height='100vh' margin='30vh 0px'>
+        <h1> Session 2 </h1>
+      </BoxLine>
+      <BoxLine height='100vh'>
+      <h1> Session 3</h1>
+      </BoxLine>
+      <BoxLine height='15vh' bg_color='black' position='absolute' left ='0px' margin='15vh 0px 0px 0px '>
+        <h3> footer</h3>
       </BoxLine>
     </>
   )
